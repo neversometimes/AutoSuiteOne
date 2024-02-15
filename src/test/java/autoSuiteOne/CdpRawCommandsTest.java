e144b026135ac1f8b378eef4c98fdbb2f5a68625
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-02-15 02:50:09 -0700

    Added device emulation test method.

--- src/test/java/autoSuiteOne/CdpRawCommandsTest.java
+++ src/test/java/autoSuiteOne/CdpRawCommandsTest.java
@@ -182,7 +182,29 @@ public class CdpRawCommandsTest {
 
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
         assertTrue(driver.getTitle().contains("Selenium WebDriver"));
+    }
 
+    @Test
+    void testDeviceEmulation() {
+        // 1. Override user agent (Apple iPhone 6)
+        String userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X)"
+                + "AppleWebKit/600.1.3 (KHTML, like Gecko)"
+                + "Version/8.0 Mobile/12A4345d Safari/600.1.4";
+
+        devTools.send(Network.setUserAgentOverride(userAgent, Optional.empty(),
+                Optional.empty(), Optional.empty())); // override user agent for
+                                                                                // emulating iPhone 6
+        // 2. Emulate device dimension
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 375);
+        deviceMetrics.put("height", 667);
+        deviceMetrics.put("mobile", true);
+        deviceMetrics.put("deviceScaleFactor", 2);
+        ((ChromeDriver) driver).executeCdpCommand(
+                "Emulation.setDeviceMetricsOverride", deviceMetrics); // override device screen params
+
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertTrue(driver.getTitle().contains("Selenium WebDriver"));
     }
 
 } // end class
