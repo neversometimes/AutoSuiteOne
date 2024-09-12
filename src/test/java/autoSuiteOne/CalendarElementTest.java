08152cc9971651a5f89e8ffac3ee5ab19311a6be
Brian <neversometimes@icloud.com>
2024-09-11 18:35:19 -0700

    init commit.

---
+++ src/test/java/autoSuiteOne/CalendarElementTest.java
@@ -0,0 +1,74 @@
+package autoSuiteOne;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import static org.testng.Assert.*;
+
+import java.time.Duration;
+
+public class CalendarElementTest {
+    WebDriver driver;
+    SoftAssert sa = new SoftAssert();
+
+    @BeforeClass
+    public void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));  // basic implicit wait
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCalendarElement() {
+        // Exercise: set a date using a web calendar element
+        //  Task: set date to: 15 June 2027
+        //  assert date set is correct after using calendar element
+
+        String day = "15";
+        String month = "June";
+        String year = "2027";
+
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");  // start at target page!!
+
+        // click on calendar text input to bring up calendar date picker
+        driver.findElement(By.cssSelector(".react-date-picker__inputGroup")).click();
+
+        // click TWICE on month YYYY header of calendar picker
+        driver.findElement(By.cssSelector("button[class='react-calendar__navigation__label']")).click();
+        driver.findElement(By.cssSelector("button[class='react-calendar__navigation__label']")).click();
+
+        // click on "2027" in calendar year picker
+        driver.findElement(By.xpath("//button[normalize-space()='" + year + "']")).click();
+
+
+        // click "June" from calendar month picker
+        driver.findElement((By.xpath("//button[contains(.,'" + month + "')]"))).click();
+
+
+        // click "15" in calendar day picker
+        driver.findElement(By.xpath("//button[contains(.,'" + day + "')]")).click();
+
+        // assert that the new date picked is now correctly showing in the element
+        String yearTxt = driver.findElement(By.cssSelector("input[name*='year']")).getAttribute("value");
+        String monthTxt = driver.findElement(By.cssSelector("input[name*='month']")).getAttribute("value");
+        String dayTxt = driver.findElement(By.cssSelector("input[name*='day']")).getAttribute("value");
+
+        assertEquals(monthTxt + "/" + dayTxt + "/" + yearTxt, "6/15/2027");
+
+    }
+}
